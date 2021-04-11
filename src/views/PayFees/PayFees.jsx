41ae7cdc5900fe8c3088a8c1bb909069f6577501
abhipatel7 <abhipatel7420@gmail.com>
2021-04-11 14:47:37 +0530

    stripe integrated

--- src/views/PayFees/PayFees.jsx
+++ src/views/PayFees/PayFees.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import logo from '../../assets/img/StudyMateNabBar.svg';
 
-export default function PayFees() {
+const PayFees = () => {
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
   return (
-    <div>Pay Fees</div>
+    <StripeCheckout
+      label="Pay Now"
+      name="StudyMate"
+      billingAddress
+      shippingAddress
+      image={logo}
+      description="Your fees is 450$"
+      amount={450 * 100}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey="pk_test_51I6evYLKx272rZ2P4hFJnYkalLuFHejKdfLPDf24l7bjeWzjhcX0hMeSGQszMsUYpHZlN717gpjTpiycnhBnCay100x1QI4rPk"
+    />
   );
-}
+};
+
+export default PayFees;
