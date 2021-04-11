6e05fb8a3bd817e20808351627b4acbc7e323c55
anirudh1713 <anirudhatodaria11@gmail.com>
2021-04-11 12:32:41 +0530

    update pay fees ui

--- src/views/PayFees/PayFees.jsx
+++ src/views/PayFees/PayFees.jsx
@@ -4,23 +4,25 @@ import logo from '../../assets/img/StudyMateNabBar.svg';
 
 const PayFees = () => {
   const onToken = (token) => {
-    console.log(token);
     alert('Payment Successful');
   };
 
   return (
-    <StripeCheckout
-      label="Pay Now"
-      name="StudyMate"
-      billingAddress
-      shippingAddress
-      image={logo}
-      description="Your fees is 450$"
-      amount={450 * 100}
-      panelLabel="Pay Now"
-      token={onToken}
-      stripeKey="pk_test_51I6evYLKx272rZ2P4hFJnYkalLuFHejKdfLPDf24l7bjeWzjhcX0hMeSGQszMsUYpHZlN717gpjTpiycnhBnCay100x1QI4rPk"
-    />
+    <div className="h-full w-full flex flex-col justify-center items-center space-y-3">
+      <div className="text-lg font-semibold">Haven&apos;t paid fees yet?</div>
+      <StripeCheckout
+        label="Pay Now"
+        name="StudyMate"
+        billingAddress
+        shippingAddress
+        image={logo}
+        description="Your fees is 450$"
+        amount={450 * 100}
+        panelLabel="Pay Now"
+        token={onToken}
+        stripeKey="pk_test_51I6evYLKx272rZ2P4hFJnYkalLuFHejKdfLPDf24l7bjeWzjhcX0hMeSGQszMsUYpHZlN717gpjTpiycnhBnCay100x1QI4rPk"
+      />
+    </div>
   );
 };
 
